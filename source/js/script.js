8f30d6beb93ddb1f8618e086e9a48d81c87f74af
Aleksei Bugaev <103301@mail.ru>
2019-12-22 15:49:23 +0300

    finish hover effect

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,36 @@
 let navMain = document.querySelector('.header');
 let navButton = document.querySelector('.header__menu-opener');
+let modal = document.querySelector('.modal-container');
+let openModal = document.querySelector('.product-info__button');
+let openModalcart = document.querySelectorAll('.catalog-goods__cart');
+let closeModal = document.querySelector('.modal__add-to-cart');
 
 navMain.classList.remove('header--no-js');
 
 navButton.addEventListener('click', function () {
-  if(navMain.classList.contains('header--opened')) {
-    navMain.classList.remove('header--opened');
-  } else {
-  navMain.classList.add('header--opened')
-  }
+  navMain.classList.toggle('header--opened')
 });
+
+try {
+openModal.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modal.classList.add('visible');
+  });
+} catch (e) {
+  console.log(e)
+}
+
+
+for(let i = 0; i < openModalcart.length; i++) {
+
+openModalcart[i].addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.add('visible');
+});
+}
+
+closeModal.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.remove('visible');
+});
+
